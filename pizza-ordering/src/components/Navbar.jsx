7c15485e1e7d2ec46d7460a41218742fa6861dbe
Alex Walker <alex.walker@indicio.tech>
2023-01-29 11:03:10 +0100

    feat: a little peeved about navbar styling not being responsive
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- pizza-ordering/src/components/Navbar.jsx
+++ pizza-ordering/src/components/Navbar.jsx
@@ -6,15 +6,38 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+          <Image
+            className={styles.telephone}
+            src="/img/telephone.png"
+            alt=""
+            width="32"
+            height="32"
+          />
         </div>
         <div className={styles.text}>
           <div className={styles.text}>Order Now!</div>
           <div className={styles.text}>503 123 4567</div>
         </div>
       </div>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
+      <center>
+        <div className={styles.item}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>Home</li>
+            <li className={styles.listItem}>Products</li>
+            <li className={styles.listItem}>Menu</li>
+            <Image src="/img/logo.png" alt="" width="160" height="69" />
+            <li className={styles.listItem}>Events</li>
+            <li className={styles.listItem}>Blog</li>
+            <li className={styles.listItem}>Contact</li>
+          </ul>
+        </div>
+      </center>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };

6f5d1e6ee8aa318bd4423c15b5dc31140beaef41
Alex Walker <alex.walker@indicio.tech>
2023-01-29 11:55:39 +0100

    feat: embarassed x2
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- pizza-ordering/src/components/Footer.jsx
+++ pizza-ordering/src/components/Footer.jsx
@@ -1,5 +1,28 @@
+import styles from "../styles/Footer.module.css";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" alt="" layout="fill" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>Well-baked, slice of heaven.</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Find our Restaurants</h1>
+          <p className={styles.text}>
+            1654 R. Don Road
+            <br /> PizzaTown, 1001
+            <br /> 503 123 4567
+          </p>
+        </div>
+        <div className={styles.card}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

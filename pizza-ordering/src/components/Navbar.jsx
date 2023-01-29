6fb23692163c23795bc66931ea2da09ccdf04c3a
Alex Walker <alex.walker@indicio.tech>
2023-01-29 10:32:19 +0100

    feat: navbar
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- pizza-ordering/src/components/Navbar.jsx
+++ pizza-ordering/src/components/Navbar.jsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.text}>
+          <div className={styles.text}>Order Now!</div>
+          <div className={styles.text}>503 123 4567</div>
+        </div>
+      </div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
 };
 
 export default Navbar;
